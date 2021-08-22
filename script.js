44246b0221b1caf26bc72c530245bb5c41f19b24
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2021-08-22 18:43:10 +0000

    Completed program: shuffled onclick

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 var form = document.getElementById("form");
 form.addEventListener("submit", function(e) {
   e.preventDefault();
+  var inputlist = document.getElementsByClassName("input");
+  var arr = [];
+
+  for (var i = 0; i < inputlist.length; i++) {
+    var element = inputlist[i];
+    arr.push(element.value);
+  }
+
+  for (var i = 0; i < arr.length; i++) {
+    var index = Math.floor(Math.random() * arr.length);
+    var cur = arr[i];
+    arr[i] = arr[index];
+    arr[index] = cur;
+  }
+
+  var better = "Shuffled: ";
+  for (var i = 0; i < arr.length; i++) {
+    better += arr[i];
+    if (i + 1 != arr.length) {
+      better += ", ";
+    }
+  }
+
+  document.getElementById("answer").innerHTML = better;
 });
 
 var num = 1;
@@ -13,6 +37,7 @@ function addOption() {
   input.type = "text";
   input.id = "input" + num;
   input.className = "input";
+  input.required = true;
 
   var label = document.createElement("label");
   label.htmlFor = "input" + num;
